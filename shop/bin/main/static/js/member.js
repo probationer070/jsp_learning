423150f1a42b5ee959a98a9884ce657edd06bda0
Jaehwan <81615183+probationer070@users.noreply.github.com>
2024-06-11 18:05:01 +0900

    update Auth
    Auth mode update

--- shop/bin/main/static/js/member.js
+++ shop/bin/main/static/js/member.js
@@ -4,17 +4,33 @@
 
 $().ready(function(){
 	var idSubmitChk = 1; 
+	var pwcheck=false;
 	
 	$('#submitTop').on('click', function(){
-		if(chkValidate()) {
-			$("form[name=form1]").attr('action', 'loginProc');
-			$("form[name=form1]").submit();
+		if(chkValidate('LoginForm')) {
+			$("form[name=LoginForm]").attr('action', 'loginProc');
+			$("form[name=LoginForm]").submit();
 		}
 	})
 	
 	$('#logout_btn').on('click', function(){
-		if(chkValidate) {
-			$("form[name=form1]").attr('action', 'logoutProc');
+		if(chkValidate('LoginForm')) {
+			$("form[name=LoginForm]").attr('action', 'logoutProc');
+			$("form[name=LoginForm]").submit();
+		}
+	})
+	
+	$('#submit1').on('click', function(){
+		if(chkValidate('form1')) {
+			if(idSubmitChk==1){
+				alert('아이디 중복\n다시 확인해 주세요');
+				$('#idchk').focus();
+				return false;
+			}
+			if(!pwcheck) {	// pwcheck가 다르면 false이므로 참이어야 안으로 들어감
+				alert("패스워드가 다릅니다.");
+				return false;
+			}
 			$("form[name=form1]").submit();
 		}
 	})
@@ -32,29 +48,43 @@ $().ready(function(){
 					$('font[id=warning]').attr('color', 'red');
 					$('font[id=warning]').text('이미 존재하는 아이디 입니다');
 					$('#idchk').focus();
-					idSubmitChk = 1;
+					idSubmitChk = 1;	// 전송블기
 				} else {
 					$('font[id=warning]').text('');
 					$('font[id=warning]').attr('color', 'blue');
 					$('font[id=warning]').text('사용가능한 아이디 입니다');
 					$('#idchk').focus();
-					idSubmitChk = 0;
+					idSubmitChk = 0;	// 전송가능
 				}
 			}
 		})
-		
 	})
+	
+	$('#check1, #check2').keyup(function(){
+		$('font[id=check]').text('');	// 글씨 쓰는 곳 지우기
+		if($('#check1').val()!=$('#check2').val()) {
+			$('font[id=check]').text('');
+			$('font[id=check]').attr('color', 'red');
+			$('font[id=check]').text('패스워드가 다릅니다');
+			pwcheck=false;
+		} else {
+			$('font[id=check]').text('');
+			$('font[id=check]').attr('color', 'green');
+			$('font[id=check]').text('동일한 패스워드');
+			pwcheck=true;
+		}
+	});
 })
 
-function chkValidate() {
-	var flen = $("form[name=form1] .chk").length;
+function chkValidate(formNm) {
+	var flen = $("form[name="+formNm+"] .chk").length;
 	for (var i=0; i < flen; i++) {
-		if( $('.chk').eq(i).val() == "" || 
-			$('.chk').eq(i).val() == null ||
-			$('.chk').eq(i).val().trim() == "") {
-				alert($('.chk').eq(i).attr('title')+' 은/는 필수입력입니다.');
-				$('.chk').eq(i).focus();
-				return false;
+		if( $("form[name="+formNm+"] .chk").eq(i).val() == "" 
+		|| $("form[name="+formNm+"] .chk").eq(i).val() == null 
+		|| $("form[name="+formNm+"] .chk").eq(i).val().trim() == "") {
+			alert($("form[name="+formNm+"] .chk").eq(i).attr('title')+' 은/는 필수입력입니다.');
+			$("form[name="+formNm+"] .chk").eq(i).focus();
+			return false;
 		}
 	}
 	return true;
--- shop/bin/main/static/js/zipCheck.js
+++ shop/bin/main/static/js/zipCheck.js
@@ -2,7 +2,7 @@
 주소 찾기 체크 스크립트
 */
 
-function sample6_execDaumPostcode() {
+function zipCheck() {
     new daum.Postcode({
         oncomplete: function(data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
